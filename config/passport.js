c155686db8d212c9c3d543d4f63ee3980a463127
moeabdol <mohd.a.saed@gmail.com>
2017-08-30 12:49:11 +0300

    Configure passport

--- config/passport.js
+++ config/passport.js
@@ -1,2 +1,17 @@
 const JwtStrategy = require("passport-jwt").Strategy;
-const ExtractJwt = require("passport-jwt").ExtractJwt;
+const ExtractJwt  = require("passport-jwt").ExtractJwt;
+const User        = require("../models/user");
+const config      = require("./config");
+
+module.exports = function(passport) {
+  let opts = {};
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithSchema("JWT");
+  opts.secretOrKey = config.secret;
+  passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+    User.findById(jwt_payload._doc._id, (err, user) => {
+      if (err) done(err, false);
+      if (!user) done(null, false);
+      done(null, user);
+    });
+  }));
+};
